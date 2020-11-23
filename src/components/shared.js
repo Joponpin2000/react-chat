5d215514b65ec3a561dd6edd90875b486bb1db84
Idowu Joseph <idowuayanfeoluwa@gmail.com>
2020-11-24 00:11:23 +0100

    ...

--- src/components/shared.js
+++ src/components/shared.js
@@ -1,4 +1,4 @@
 export const avatarUrl = "https://avatars.dicebar.com/api/human/";
 
 // export const ENDPOINT = 'localhost:5000';
-export const ENDPOINT = 'https://pluks.herokuapp.com/';
+export const ENDPOINT = 'http://pluks.herokuapp.com/';
