f65c36ef00c75b5cc1094c5346abdcc1c8c0fbb9
Idowu Joseph <idowuayanfeoluwa@gmail.com>
2020-11-24 00:00:31 +0100

    ...

--- src/components/shared.js
+++ src/components/shared.js
@@ -1,4 +1,4 @@
 export const avatarUrl = "https://avatars.dicebar.com/api/human/";
 
 // export const ENDPOINT = 'localhost:5000';
-export const ENDPOINT = 'https://mern-chat-app.heroku.com/';
+export const ENDPOINT = 'https://pluks.herokuapp.com/';
